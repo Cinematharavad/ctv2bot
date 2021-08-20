746005ba5eab8ebc9430677f9e47d552a272c001
Cinematharavad <87141372+Cinematharavad@users.noreply.github.com>
2021-08-20 18:31:22 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} /n @cinema_tharavad",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '⚠️ OUR GROUP ⚠️', url="https://t.me/cinema_tharavad"
                                 )
                         ]
                     ]
