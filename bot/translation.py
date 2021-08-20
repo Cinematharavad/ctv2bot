154b2e11e81d6326899bb297f6655d28a546ad5c
Cinematharavad <87141372+Cinematharavad@users.noreply.github.com>
2021-08-20 18:25:17 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,13 +4,13 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+    START_TEXT = """<b>Hey {} How Are you!!</b>
+<i>ഞാൻ ഒരു ബോട്ട് ആൺ എന്ന് നിങ്ങൾക്ക് അറിയും ലെ😢
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+YOU CANT ADD ME TO YOUR GROUPS😎
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@cinema_tharavad
 </i>"""    
     
     HELP_TEXT = """
@@ -48,11 +48,11 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Oru_Budhishaali">IRON MAN</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a https://github.com/Cinematharavad/ctv2bot Me</a></i>
 """
